c24526861edcce446aeed092d18fb77453179525
Jon Penton <hey@jondpenton.com>
2020-10-01 12:32:46 -0400

    feat: :sparkles: finished lesson 2

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,21 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require('../math')
 
 let result, expected
 
 result = sum(3, 7)
 expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+  }
 }
